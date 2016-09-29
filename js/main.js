25316dc6f8ae098f1b0b322ac9aa763602038479
zmx2321@163.com <zmx2321@163.com>
2016-09-29 08:45:47 +0800

    更新

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
 $(function(){
 	
 });
+
+var selected_val = document.getElementById(select_id).value;
+var sel = document.getElementById(select_id);
+var selected_val = sel.options[sel.selectedIndex].value;
