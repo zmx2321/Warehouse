5cef3212ffb37d4679c7c393979d1c04018721e2
zmx2321@163.com <zmx2321@163.com>
2016-10-18 17:39:29 +0800

    gx

---
+++ classify/angularDemo/project/mvc/js/main.js
@@ -0,0 +1,15 @@
+/*var myModule = angular.module("HelloAngular",[]);
+
+myModule.controller("HelloAngular",['$scope',
+	function HelloAngular($scope){
+		$scope.greeting = {
+			text: 'Hello'
+		};
+	}
+]);*/
+
+function HelloAngular($scope) {
+  $scope.greeting = {
+    text: 'Hello'
+  };
+}
