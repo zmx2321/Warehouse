9a2905aaf31b670a22af81ee1f8268139be84b27
zmx2321@163.com <zmx2321@163.com>
2016-11-14 17:00:41 +0800

    gx

--- classify/vueDemo/project/hello_vue/js/main1.js
+++ classify/vueDemo/project/hello_vue/js/main1.js
@@ -31,3 +31,22 @@ var app4 = new Vue({
 		]
 	}
 });
+
+var app5 = new Vue({
+  el: '#app-5',
+  data: {
+    message: 'Hello Vue.js!'
+  },
+  methods: {
+    reverseMessage: function () {
+      this.message = this.message.split('').reverse().join('')
+    }
+  }
+});
+
+var app6 = new Vue({
+  el: '#app-6',
+  data: {
+    message: 'Hello Vue!'
+  }
+});
