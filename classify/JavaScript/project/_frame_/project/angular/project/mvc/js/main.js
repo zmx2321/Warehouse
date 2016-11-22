153ecf7659b1b4e947189448adfe042d4ceed547
zmx2321@163.com <zmx2321@163.com>
2016-11-22 11:24:44 +0800

    gx

--- classify/JavaScript/project/_frame_/project/angular/project/mvc/js/main.js
+++ classify/JavaScript/project/_frame_/project/angular/project/mvc/js/main.js
@@ -1,5 +1,15 @@
-function HelloAngular($scope) {
+/*function HelloAngular($scope) {
 	$scope.greeting = {
 		text: 'Hello'
 	};
-}
+}*/
+
+var myModule = angular.module("HelloAngular", []);
+
+myModule.controller("HelloAngular", 
+	function HelloAngular($scope) {
+		$scope.greeting = {
+			text: 'Hello'
+		};
+	}
+);
